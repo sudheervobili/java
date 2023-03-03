b9abb564bdd0a1d5be1ac170c9f529fe09270936
sudheervobili <108193213+sudheervobili@users.noreply.github.com>
2023-03-03 22:44:23 +0530

    bufferstring

---
+++ First.java
@@ -0,0 +1,12 @@
+//import java.lang.package;
+
+public class First
+{
+  public static void main(String[] args)
+  { 
+     StringBuffer sb  = new StringBuffer("Java");
+	 sb.append(" Programming");
+	 sb.delete(7,12);
+	 System.out.println(sb);
+  }
+}
